93ee68abed001d2a5ee7fe57ad6d82fccf38381b
Нина <gvrnskaya@yandex.ru>
2025-05-18 23:41:06 +0300

    update of pep8

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
@@ -11,4 +10,3 @@ urlpatterns = [
         name='redoc'
     ),
 ]
-
